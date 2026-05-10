4cce8c081525040c0798056f51c162425a6753af
ayushkothari1 <pahadiayush61@gmail.com>
2026-05-10 20:27:23 +0530

    reactstart

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <h1>
+      Chair aur react | I learned that we can initialize react by eighter create
+      react project way or vite and vercel way and usually vite way is
+      preferred, because create react is bulky and vite is in bundle
+    </h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return (
+   <h1>Chair aur react with vite | Ayush</h1>
+  )
+}
+
+export default App
