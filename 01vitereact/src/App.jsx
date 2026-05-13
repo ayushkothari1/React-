97332db7a4463bb454817c8672957e52d6dedeee
ayushkothari1 <pahadiayush61@gmail.com>
2026-05-13 16:40:28 +0530

    created own semi react library

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,11 @@
 import Chai from "./chai"
 
 function App() {
-
+const userName = 'chai aur code'
   return (
     <>
 <Chai/>
-<h1>Me and react</h1>
+<h1>Me and react {userName}</h1>
 <p>test para</p>
 </>
   )
